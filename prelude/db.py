1a5c55f3966ce34791d9ad8981045d30979f74d1
Jussi Asp <jussi.asp@helsinki.fi>
2021-09-26 21:28:06 +0300

    database string connection fix with heroku.

--- prelude/db.py
+++ prelude/db.py
@@ -3,7 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from models import User
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+
+# fix heroku postgres / postgresql -problem
+import os
+import re
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# fix end
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+# app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
